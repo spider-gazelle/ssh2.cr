066996576bd11e1b0eb6d44e26c9f42b4aa0fd05
Kent Sibilev <ksibilev@gmail.com>
2015-05-29 12:57:13 -0400

    ditto

--- crash.cr
+++ crash.cr
@@ -1,12 +1,7 @@
 require "./src/ssh2"
 
 SSH2::Session.open("localhost", 2222) do |session|
-  session.login_with_pubkey("root", "./spec/keys/id_rsa")
   session.open_session do |ch|
-    ch.request_pty("vanilla")
-    ch.shell
-    session.blocking = false
-
     # THE FOLLOWING LINE CRASHES COMPILER
     loop {}
   end
