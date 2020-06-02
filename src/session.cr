d6dc79770d66186e4b5558d4789cf216e9f8dd4c
Stephen von Takach <steve@acaprojects.com>
2020-06-03 09:04:52 +1000

    fix: raise a SSH2Error

--- src/session.cr
+++ src/session.cr
@@ -404,7 +404,7 @@ class SSH2::Session
   # Send a file from a local filesystem to the remote host via SCP.
   def scp_send_file(path)
     if LibC.stat(path, out stat) != 0
-      raise ("Unable to get stat for '#{path}'")
+      raise SSH2Error.new("Unable to get stat for '#{path}'")
     end
     scp_send(path, (stat.st_mode & 0x3ff).to_i32, stat.st_size.to_u64,
       stat.st_mtim.tv_sec, stat.st_atim.tv_sec) do |ch|
